0d4b4dadebf284ee74cc3159528cae390a73a3a8
ruhul2003 <ruhul941020@gmail.com>
2026-05-21 17:13:43 +0600

    Route path error fixed

--- src/app/api/auth/[...better-auth]/route.js
+++ src/app/api/auth/[...better-auth]/route.js
@@ -1,4 +1,7 @@
-import { auth } from "@/lib/auth";
+import { auth } from "@/lib/auth"; 
+import { toNextJsHandler } from "better-auth/next-js";
 
-export const GET = auth.handler;
-export const POST = auth.handler;
+const handler = toNextJsHandler(auth);
+
+export const GET = handler;
+export const POST = handler;
